ebfa6295d2df1b198967e8bb3fe4ef4b5188c303
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-09-22 09:00:06 +0300

    added the Doctors list and doctor card component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,6 +13,7 @@ import { Link } from 'react-router-dom';
 import { BsArrowRight } from 'react-icons/bs';
 import About from '../components/About/About';
 import ServiceList from '../components/Services/ServiceList';
+import DoctorList from '../components/Doctors/DoctorsList';
 
 
 // import DoctorList from '../components/Doctors/DoctorList';
@@ -223,7 +224,7 @@ const Home = () => {
                         <h2 className="heading text-center">Our Expert Doctors</h2>
                         <p className='text__para text-center'>World-class care for everyone. Our health system offers unmatched, expert health care.</p>
                     </div>
-                    {/* <DoctorList /> */}
+                    <DoctorList />
                 </div>
             </section>
 
