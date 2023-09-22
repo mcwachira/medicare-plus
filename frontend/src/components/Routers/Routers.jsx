095e2edea08b855db60cc00ba86bb1a22fd8fba7
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-09-22 07:52:51 +0300

    created the Header and added some images

---
+++ frontend/src/components/Routers/Routers.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../../pages/Home'
+import Doctors from '../../pages/Doctors/Doctors'
+import DoctorDetails from '../../pages/Doctors/DoctorDetails'
+import Login from '../../pages/Login'
+import Contact from '../../pages/Contact'
+import SignUp from '../../pages/SignUp'
+import Services from '../../pages/Services'
+
+const Routers = () => {
+  return (
+ <Routes>
+
+
+    <Route path='/' element={<Home/>}/>
+    <Route path='/Home' element={<Home/>}/>
+    <Route path='/doctors' element={<Doctors/>}/>
+
+
+    <Route path='/doctor/:id' element={<DoctorDetails/>}/>
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/contact' element={<Contact/>}/>
+
+
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/services' element={<Services/>}/>
+
+
+ </Routes>
+  )
+}
+
+export default Routers
