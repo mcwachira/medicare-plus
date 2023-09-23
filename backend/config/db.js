f5b4c509ca2dba11804d3679f4387779e7b01f15
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-09-23 11:11:13 +0300

    initialized the backend  and created my models

---
+++ backend/config/db.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import colors from 'colors'
+
+const connectDb = async () => {
+    console.log(process.env.MONGO_URI)
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            name: 'Medicare Backend',
+        })
+
+        console.log(`MongoDb  connected ${connect.connection.host}`.blue.underline)
+    } catch (error) {
+        console.log(`error :${error.messages}`.red.underline.bold)
+        process.exit(1)
+    }
+
+
+}
+
+
+export default  connectDb;
---
+++ backend/server.js
@@ -0,0 +1,62 @@
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+// import { notFound, errorHandler} from './middleware/errorMiddleware.js'
+// import products from './data/products.js'
+import dotenv from 'dotenv'
+
+dotenv.config();
+import  connectDb from './config/db.js'
+import path from 'path'
+
+
+
+
+
+const port = process.env.PORT  || 5000;
+
+//mongodb database
+connectDb()
+const app = express();
+
+// for parsing application/json
+app.use(express.json({ limit: "30mb", extended: true }))
+// for parsing application/x-www-form-urlencoded /form data
+app.use(express.urlencoded({ limit: "30mb", extended: true }))
+
+//enabling helmet
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+
+//enabling cors
+app.use(cors())
+
+//enables us to se logs in our terminal
+app.use(morgan('tiny')) //used to log request from the frontend
+//get cookies
+app.use(cookieParser())
+
+
+
+const __dirname = path.resolve(); //sets __dirname to current directory
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if(process.env.NODE_ENV ==='production'){
+    //set static folder
+
+    app.use(express.static(path.join(__dirname, '/client/build')))
+
+    //any routes that is not in the api will be redirect to index.html
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}else{
+    app.get('/', (req, res) => {
+        res.send('api IS RUNNING ...')
+    })
+
+}
+//error handler
+app.use(notFound);
+app.use(errorHandler)
+app.listen(port, () => console.log(`server is running on port ${port}`))
